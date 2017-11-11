b3ac4e40101a8102da2db43c70d957368c41ad30
Ravishen Jain <ravisshen@gmail.com>
2017-11-11 23:02:33 +0530

    I dont think its going to work

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get '/home', to: 'pages#home'
-root to: "pages#home"
+  get '/index', to: 'pages#index'
+  get '/users/:id', to: 'pages#index'
+  root to: "pages#index"
 end
