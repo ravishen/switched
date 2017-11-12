172ab0a6466b2d9f26cdb9ff0bbcd944c635933b
Ravishen Jain <ravisshen@gmail.com>
2017-11-12 17:50:29 +0530

    modified pages controller to work with postgres

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,13 @@ class PagesController < ApplicationController
   end
 
   def index
-    if user_signed_in?
+    if user_signed_in?&&Check.nil?
     @checks=Check.find(current_user.id)
     @users=User.all
+    else
+    @checks=Check.new()
+    @users=User.all
+
     end
   end
 end
